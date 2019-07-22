434a2a575af410461f8884d351ae786e0fc6911d
Misa <dieuhuongngo@gmail.com>
2019-07-22 10:34:13 +0300

    1.4: course information step4

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-const Total = props => <p>Number of exercises {props.total}</p>;
+const Total = props => (
+  <p>
+    Number of exercises{" "}
+    {props.parts[0].exercises +
+      props.parts[1].exercises +
+      props.parts[2].exercises}
+  </p>
+);
 
 export default Total;
