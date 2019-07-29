cc2cf1656cc934737ee8e7e1049a38179f22a0fb
Misa <dieuhuongngo@gmail.com>
2019-07-29 16:37:32 +0300

    2.2: Course contents step7

--- part1/courseinfo/src/Content.js
+++ part1/courseinfo/src/Content.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Part from "./Part";
 
-const Content = props => (
-  <>
-    <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-    <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-    <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-  </>
-);
+const Content = ({ parts }) =>
+  parts.map(part => (
+    <Part key={part.name} part={part.name} exercises={part.exercises} />
+  ));
 
 export default Content;
--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 const Total = props => (
-  <p>
-    Number of exercises{" "}
+  <p style={{ fontWeight: "bold" }}>
+    Total of{" "}
     {props.parts[0].exercises +
       props.parts[1].exercises +
-      props.parts[2].exercises}
+      props.parts[2].exercises}{" "}
+    exercises
   </p>
 );
 
