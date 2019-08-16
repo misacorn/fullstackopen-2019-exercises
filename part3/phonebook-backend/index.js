96693de46397bf225d6ae491e783494a79aace17
Misa <dieuhuongngo@gmail.com>
2019-08-16 19:21:34 +0300

    3.4: Phonebook backend step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -38,6 +38,12 @@ app.get("/api/persons/:id", (req, res) => {
   person ? res.json(person) : res.status(404).end();
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
+});
+
 app.get("/info", (req, res) => {
   const date = new Date();
   res.send(
