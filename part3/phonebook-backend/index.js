7240655131c89a12b31345710a5503f76c332e62
Misa <dieuhuongngo@gmail.com>
2019-08-26 15:48:09 +0300

    3.9 phonebook backend step9

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 
 morgan.token("post", function(req, res) {
@@ -93,7 +96,7 @@ app.post("/api/persons/", (req, res) => {
   res.json(newPerson);
 });
 
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
