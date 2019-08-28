66059b8575fb09a6b37aafb6886362f890b96adf
Misa <dieuhuongngo@gmail.com>
2019-08-28 10:22:42 +0300

    adding a production build

---
+++ part2/phonebook/build/precache-manifest.8ccd69fff73f2d507ebff9528523455a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4b8bd9623528c9d6cbfa0e9553a0e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7089c4198d6da07edf13",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "962a455d30389fe78659",
+    "url": "/static/js/2.37de1747.chunk.js"
+  },
+  {
+    "revision": "7089c4198d6da07edf13",
+    "url": "/static/js/main.c20d4cf4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ part3/phonebook-backend/build/precache-manifest.006623c40ff2a6bf8ee15186eaa82bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d637f8162a7e29fab182e84706473207",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a679b2bfe22ca26fc7e5",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "962a455d30389fe78659",
+    "url": "/static/js/2.37de1747.chunk.js"
+  },
+  {
+    "revision": "a679b2bfe22ca26fc7e5",
+    "url": "/static/js/main.b514bb7d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
