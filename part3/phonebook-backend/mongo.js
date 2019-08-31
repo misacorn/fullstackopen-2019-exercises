d860828cca9ab6934f651a3437292cdcffd36c87
Misa <dieuhuongngo@gmail.com>
2019-08-31 22:51:31 +0300

    3.12: Command-line database

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://Misa:${password}@cluster0-leek9.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model("Person", noteSchema);
+
+if (process.argv[3]) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4]
+  });
+
+  person.save().then(response => {
+    console.log(`added ${person.name} ${person.number}`);
+    mongoose.connection.close();
+  });
+} else {
+  console.log("phonebook:");
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
