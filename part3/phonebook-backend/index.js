7c1f2ffbfd636e4675ae5b03c76f9d881223a56d
Misa <dieuhuongngo@gmail.com>
2019-09-02 13:02:18 +0300

    3.17*: Phonebook database, step4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -70,6 +70,19 @@ app.post("/api/persons/", (req, res) => {
   });
 });
 
+app.put("/api/persons/:id", (req, res, next) => {
+  const body = req.body;
+  const person = {
+    name: body.name,
+    number: body.number
+  };
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON());
+    })
+    .catch(error => next(error));
+});
+
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
 };
