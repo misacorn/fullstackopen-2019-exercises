f0682dfdfdfa4ed9caf72917a418bdb39ce4454a
Misa <dieuhuongngo@gmail.com>
2019-09-09 12:45:24 +0300

    trial test with Favourite Blog

---
+++ part4/bloglist/test/favoriteBlog.test.js
@@ -0,0 +1,32 @@
+const listHelper = require("../utils/list_helper");
+
+describe("most likes", () => {
+  const listWithThreeBlogs = [
+    {
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      id: "5d728cb8e0becf1d7b636c10"
+    },
+    {
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      id: "5d728cd9e0becf1d7b636c11"
+    },
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      id: "5d760883fc16f820ee57338b"
+    }
+  ];
+  test("which blog has most likes", () => {
+    const result = listHelper.favoriteBlog(listWithThreeBlogs);
+    expect(result).toBe(12);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,26 @@
-const dummy = blogs => {
+const dummy = () => {
   return 1;
 };
 
+const totalLikes = blogs => {
+  const reducer = (sum, like) => {
+    return sum + like;
+  };
+  return blogs.length === 0
+    ? 0
+    : blogs.map(blog => blog.likes).reduce(reducer, 0);
+};
+
+const favoriteBlog = blogs => {
+  return blogs.length === 0 ? 0 : Math.max(...blogs.map(blog => blog.likes));
+  // const max = blogs.reduce((prev, current) =>
+  //   prev.likes > current.likes ? prev : current
+  // );
+  // return blogs.length === 0 ? 0 : max;
+};
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 };
