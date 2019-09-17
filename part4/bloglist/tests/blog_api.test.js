f1ab7f64c4a84e67ac625e5e1beb89dbf129e114
Misa <dieuhuongngo@gmail.com>
2019-09-17 22:08:22 +0300

    4.10: Blog list tests, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -32,13 +32,6 @@ beforeEach(async () => {
   await blogObject.save();
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
-
 test("amount of blogs", async () => {
   const res = await api.get("/api/blogs");
   expect(res.body.length).toBe(initialBlogs.length);
@@ -47,6 +40,27 @@ test("amount of blogs", async () => {
 test("unique identifier property of the blog posts is named id", async () => {
   const res = await api.get("/api/blogs/");
   expect(res.body[0].id).toBeDefined();
+});
+
+test("a valid blog can be added ", async () => {
+  const newBlog = {
+    title: "Learn Basics of React.js in 11 Minutes",
+    author: "Madhu Pathy",
+    url:
+      "https://medium.com/@madhupathy/learn-basics-of-react-js-in-3-minutes-a94cbc6f02c8",
+    likes: 15
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const res = await api.get("/api/blogs");
+  const author = res.body.map(r => r.author);
+  expect(res.body.length).toBe(initialBlogs.length + 1);
+  expect(author).toContain("Madhu Pathy");
 });
 
 afterAll(() => {
