562224fbf4474e3ee3e4f9d5c559bf301c4f9100
Misa <dieuhuongngo@gmail.com>
2019-09-18 10:56:48 +0300

    refractoring

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,40 +1,23 @@
 const mongoose = require("mongoose");
 const supertest = require("supertest");
+const helper = require("./test_helper");
 const app = require("../app");
 const api = supertest(app);
 const Blog = require("../models/blog");
 
-const initialBlogs = [
-  {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url:
-      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    id: "5d728cb8e0becf1d7b636c10"
-  },
-  {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    id: "5d728cd9e0becf1d7b636c11"
-  }
-];
-
 beforeEach(async () => {
   await Blog.deleteMany({});
 
-  let blogObject = new Blog(initialBlogs[0]);
+  let blogObject = new Blog(helper.initialBlogs[0]);
   await blogObject.save();
 
-  blogObject = new Blog(initialBlogs[1]);
+  blogObject = new Blog(helper.initialBlogs[1]);
   await blogObject.save();
 });
 
 test("amount of blogs", async () => {
   const res = await api.get("/api/blogs");
-  expect(res.body.length).toBe(initialBlogs.length);
+  expect(res.body.length).toBe(helper.initialBlogs.length);
 });
 
 test("unique identifier property of the blog posts is named id", async () => {
@@ -59,7 +42,7 @@ test("a valid blog can be added ", async () => {
 
   const res = await api.get("/api/blogs");
   const author = res.body.map(r => r.author);
-  expect(res.body.length).toBe(initialBlogs.length + 1);
+  expect(res.body.length).toBe(helper.initialBlogs.length + 1);
   expect(author).toContain("Madhu Pathy");
 });
 
---
+++ part4/bloglist/tests/test_helper.js
@@ -0,0 +1,29 @@
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url:
+      "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    id: "5d728cb8e0becf1d7b636c10"
+  },
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    id: "5d728cd9e0becf1d7b636c11"
+  }
+];
+
+const notesInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map(blog => blog.toJSON());
+};
+
+module.exports = {
+  initialBlogs,
+  notesInDb
+};
