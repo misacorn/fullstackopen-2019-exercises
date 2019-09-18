2702fb177b201bff42cc10dbceea1761fa3b4d1d
Misa <dieuhuongngo@gmail.com>
2019-09-18 20:37:29 +0300

    adding error handling

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,15 +2,23 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
-  res.json(blogs);
+  try {
+    const blogs = await Blog.find({});
+    res.json(blogs);
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 blogsRouter.post("/", async (req, res) => {
-  const blog = await new Blog(req.body);
-  blog.save().then(result => {
-    res.status(201).json(result);
-  });
+  try {
+    const blog = await new Blog(req.body);
+    blog.save().then(result => {
+      res.status(201).json(result);
+    });
+  } catch (exception) {
+    next(exception);
+  }
 });
 
 module.exports = blogsRouter;
