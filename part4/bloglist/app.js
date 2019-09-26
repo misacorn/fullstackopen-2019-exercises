0d9e67a8f050d1f702002706ab5d2e5a95f35e0c
Misa <dieuhuongngo@gmail.com>
2019-09-26 21:07:15 +0300

    create a user

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
 
 mongoose
   .connect(config.MONGODB_URI, { useNewUrlParser: true })
@@ -19,5 +20,6 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
