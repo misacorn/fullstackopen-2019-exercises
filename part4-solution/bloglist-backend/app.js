41d7e15b0e9e0ded7d953c4d2ee6d98402deebdb
Misa <dieuhuongngo@gmail.com>
2019-10-09 17:45:39 +0300

    solved Deprecation Warnings

--- part4-solution/bloglist-backend/app.js
+++ part4-solution/bloglist-backend/app.js
@@ -20,7 +20,8 @@ mongoose
   .connect(config.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => {
     console.log("connected to MongoDB");
