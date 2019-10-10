ebe1f077546cdb6212afd4dda5c9fc6d0e5dcb60
Misa <dieuhuongngo@gmail.com>
2019-10-10 23:38:45 +0300

    5.1: bloglist frontend, step1

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
+import Blog from "./components/Blog";
 
 import Notification from "./components/Notification";
 import loginService from "./services/login";
+import blogService from "./services/blogs";
 
 const App = () => {
+  const [blogs, setBlogs] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await blogService.getAll();
+      setBlogs(result);
+    };
+    fetchData();
+  }, []);
 
   const handleLogin = async event => {
     event.preventDefault();
@@ -64,9 +76,11 @@ const App = () => {
         <div>
           <h2>Blogs</h2>
           <p>{user.name} logged in</p>
-          {/* {blogs.map(blog => (
-            <Blog key={blog.id} blog={blog} /> */}
-          ))}
+          {blogs.map(blog =>
+            blog.user.name === user.name ? (
+              <Blog key={blog.id} blog={blog} />
+            ) : null
+          )}
         </div>
       )}
     </div>
