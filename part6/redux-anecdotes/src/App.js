059fe5f7b58db6e85de3b3a5c79b8f2bc3661e45
Misa <dieuhuongngo@gmail.com>
2019-10-29 17:42:28 +0200

    6.8: anecdotes, step6

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,36 +1,13 @@
 import React from "react";
-import { addVote } from "./reducers/anecdoteReducer";
 
 import AnecdoteForm from "./components/AnecdoteForm";
+import AnecdoteList from "./components/AnecdoteList";
 
 const App = props => {
-  const anecdotes = props.store.getState();
-
-  const vote = id => {
-    props.store.dispatch(addVote(id));
-  };
-
-  // const createNew = event => {
-  //   event.preventDefault();
-  //   const content = event.target.anecdote.value;
-  //   props.store.dispatch(addAnec(content));
-  //   event.target.anecdote.value = "";
-  // };
-
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes
-        .sort((a, b) => b.votes - a.votes)
-        .map(anecdote => (
-          <div key={anecdote.id}>
-            <div>{anecdote.content}</div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
-            </div>
-          </div>
-        ))}
+      <AnecdoteList store={props.store} />
       <AnecdoteForm store={props.store} />
     </div>
   );
