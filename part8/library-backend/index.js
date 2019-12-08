f1ce5dbbdfbf397d982622931dc989343ececb63
Misa <dieuhuongngo@gmail.com>
2019-12-08 20:02:37 +0200

    8.4: Books of an author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -102,7 +102,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -111,7 +111,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, arg) => books.filter(book => book.author === arg.author),
     allAuthors: () =>
       authors.map(author => ({
         name: author.name,
